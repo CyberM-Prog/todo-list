bb794dbd41f415c1cf80eb614f16d56f40c0156a
Martim <martim.pintao@gmail.com>
2022-06-23 12:24:59 +0100

    Create factory function that creates projects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./index.css"
 import * as userInterface from "./user-interface";
 import * as todos from "./todos"
+import * as projects from "./projects"
 
 (function createContainer() {
     const content = document.createElement("div")
---
+++ src/projects.js
@@ -0,0 +1,4 @@
+export const projectFactory = (title) => {
+    const todos = []
+    return {title, todos}
+}
