717d0b24996a1bb259b884940d1238e17f094641
Martim <martim.pintao@gmail.com>
2022-06-24 17:44:28 +0100

    Create function that handles the logic of creating a todo

--- src/index.js
+++ src/index.js
@@ -13,3 +13,40 @@ import * as projects from "./projects"
 
 userInterface.createUserInterface()
 userInterface.createInboxInterface()
+
+const inbox = projects.projectFactory("Inbox")
+let currentProject = inbox
+console.log(currentProject) // a seguir tratar da logica que mete os novos elementos na inbox
+
+const newTaskButton = document.querySelector(".newtask") 
+
+newTaskButton.addEventListener("click", () => {
+    userInterface.showNewTaskPopup()
+
+    const addTaskButton = document.querySelector(".submit")
+    addTaskButton.addEventListener("click", function() {
+        const title = document.querySelector(".title")
+        const description = document.querySelector(".description")
+        const dueDate = document.querySelector(".duedate")
+        const priorityMax = document.querySelector(".radio1")
+        const priorityMedium = document.querySelector(".radio2")
+        const priorityMin = document.querySelector(".radio3")
+        const notes = document.querySelector(".notes")
+
+        let priority
+
+        if (priorityMax.checked) priority = "Maximum"
+        if (priorityMedium.checked) priority = "Medium"
+        if (priorityMin.checked) priority = "Minimum"
+
+        const newTask = todos.todoFactory(title.value, description.value, dueDate.value, priority, notes.value)
+        currentProject.todos.push(newTask)
+        userInterface.closeNewTaskPopup()
+
+        console.log(currentProject.todos[currentProject.todos.length - 1].title)
+
+        let currentTodo = currentProject.todos[currentProject.todos.length - 1]
+
+        userInterface.showTask(currentTodo.title, currentTodo.dueDate, currentTodo.priority)
+    })
+})
